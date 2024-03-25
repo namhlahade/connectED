85f3fd6a3d909996fa67b231b4b43f2366c5cb91
Neel Runton <neelam.runton@duke.edu>
2024-03-24 22:21:45 -0400

    initial Xcode commit

---
+++ connectEd/connectEd/connectEdApp.swift
@@ -0,0 +1,17 @@
+//
+//  connectEdApp.swift
+//  connectEd
+//
+//  Created by Neel Runton on 3/24/24.
+//
+
+import SwiftUI
+
+@main
+struct connectEdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
