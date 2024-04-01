5782d3f11126a4f35d1c0e5c3c37bff328d21dbd
Nick Steinly <njs40@duke.edu>
2024-04-01 17:27:56 -0400

    began review form

--- connectEd/connectEd/Views/ReviewForm.swift
+++ connectEd/connectEd/Views/ReviewForm.swift
@@ -1,17 +1,60 @@
-//
-//  ReviewForm.swift
-//  connectEd
-//
-//  Created by Neel Runton on 4/1/24.
-//
 
 import SwiftUI
 
 struct ReviewForm: View {
+    @State private var course: String = ""
+    @State private var rating: Int = 0
+    @State private var additionalComments: String = ""
     var body: some View {
-        Text("Review Form page. This will be how users leave reviews of tutors. Should be accessed from the TutorDetails page. Will have to create a request to edit ratings of tutors.")
+        Form {
+            Section(header: Text("Course Being Tutored")) {
+                TextField("Course", text: $course)
+            }
+            
+            
+            Section(header: Text("Rating")) {
+                HStack {
+                    ForEach(1...5, id: \.self) { index in
+                        Button(action: {
+                            self.rating = index
+                        }) {
+                            Image(systemName: self.getStarImageName(for: index))
+                                .foregroundColor(self.getStarColor(for: index))
+                                .font(.system(size: 25))
+                        }
+                    }
+                }
+            }
+            
+            Section(header: Text("Additional Comments")) {
+                TextEditor(text: $additionalComments)
+                    .frame(height: 100)
+            }
+            
+            Button(action: {
+            }) {
+                Text("Submit Review")
+            }
+        }
+        .navigationBarTitle("Leave a Review")
+    }
+    private func getStarImageName(for index: Int) -> String {
+        if index <= self.rating {
+            return "star.fill"
+        } else {
+            return "star"
+        }
+    }
+
+    private func getStarColor(for index: Int) -> Color {
+        if index <= self.rating {
+            return .yellow
+        } else {
+            return .gray
+        }
     }
 }
+
 
 #Preview {
     ReviewForm()
