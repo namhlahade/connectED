f04905d6b3f95e4b7e97564f848762d543615f1f
Namh Lahade <namh.lahade@duke.edu>
2024-04-04 18:57:38 -0400

    made some changes to flask api

--- backend/app.py
+++ backend/app.py
@@ -3,23 +3,25 @@ import sqlite3
 import os
 
 app = Flask(__name__)
+app.debug = True
 
 @app.route('/testing', methods=['GET'])
 def testing():
     returnAnswer = "Things are working"
-    return jsonify(returnAnswer), 200
+    print(returnAnswer)
+    return jsonify({}), 200
 
-# Function to get the database connection
 def get_db():
-    # Construct the absolute path for the database file
     db_path = os.path.join(os.path.dirname(__file__), 'database.db')
     db = sqlite3.connect(db_path)
     return db
 
-# Endpoint to get all tutors
 @app.route('/getTutors', methods=['GET'])
 def getTutors():
     db = get_db()
     query = db.execute("select * from users")
     everything = query.fetchall()
     return jsonify(everything), 200
+
+if __name__ == '__main__':
+    app.run()
