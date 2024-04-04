22de9cc51b5f2023622bd502432cdb92308acc4a
James Qu <jqu@Jamess-MBP-2.lan>
2024-04-04 02:24:11 -0400

    Added the implementation of the Tutor model class (previous commit did not push the updated versions)

--- connectEd/connectEd/Models/Tutor.swift
+++ connectEd/connectEd/Models/Tutor.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+import SwiftData
+
+@Model
+class Tutor: Identifiable {
+    var name: String
+    var email: String?
+    var bio: String?
+    var courses: [String]
+    var image: URL?
+    var status: Status
+    var rating: Int?
+    
+    init(name: String, email: String? = nil, bio: String? = nil, courses: [String], image: URL? = nil, status: Status, rating: Int? = nil) {
+        self.name = name
+        self.email = email
+        self.bio = bio
+        self.courses = courses
+        self.image = image
+        self.status = status
+        self.rating = rating
+    }
+    
+}
+
+// Used to show if a tutor is online or offline
+enum Status {
+    case online
+    case offline
+}
