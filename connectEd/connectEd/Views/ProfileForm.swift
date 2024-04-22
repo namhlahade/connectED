818377d549ecd5177afd08369dc2dc32325cd5bc
Neel Runton <neelam.runton@duke.edu>
2024-04-21 20:39:54 -0400

    changed 'submit review' button to just 'submit'. got rid of extraneous private var in ProfileForm

--- connectEd/connectEd/Views/ProfileForm.swift
+++ connectEd/connectEd/Views/ProfileForm.swift
@@ -14,7 +14,6 @@ struct ProfileForm: View {
     
     
     @State var selectedImage: PhotosPickerItem?
-    //@State private var imageData: Data? = nil
     
     
     @Binding var data: Tutor.FormData
